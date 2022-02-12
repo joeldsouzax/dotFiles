daf02f81b85c81278c4b7f5da15b6125615e092c
Joel DSouza <joel.dsouza@seamless.se>
2022-02-12 19:54:41 +0300

    added basic emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,18 @@
+;; test configuration
+;; will need to change this later on.
+;; use emacs org configuration
+
+
+;; --------------------------
+;; BASIC CONFIG
+;; --------------------------
+
+
+(setq inhibit-startup-message t) ;; remove the default emacs startup message
+(scroll-bar-mode -1)             ;; disable visible scrollbar
+(tool-bar-mode -1)               ;; disable the toolbar
+(tooltip-mode -1)                ;; disable the tooltips
+(set-fringe-mode 10)             ;; give some breathing room :D
+(menu-bar-mode -1)               ;; disable the menu bar
+(setq visible-bell 1)            ;; set up bthe visible alert bell
+
